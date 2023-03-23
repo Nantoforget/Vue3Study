8bbcced3f16a6c18bc95cd2a404cae9e143e02e1
jiazeshuai <2896613845@qq.com>
2023-03-23 15:52:57 +0800

    响应式代理

--- 02-vite/03-auto_vite/src/App.vue
+++ 02-vite/03-auto_vite/src/App.vue
@@ -1,26 +1,29 @@
 <template>
-    <h1>组合式API</h1>
-    <h2>我是a：{{ a }}</h2>
-    <button @click="changeA">Click Me</button>
+    <h1>{{ a }}</h1>
+    <h1>{{ stu.name }}--{{ stu.age }}</h1>
 </template>
-<script>
-import { reactive, ref } from 'vue';
-//可以通过reactive()来创建一个响应的对象
-export default {
-    setup() {
-        let a = ref(10);
-        const stu = reactive({
-            name: "孙悟空",
-            age: 18
-        })
-        function changeA() {
-            a.value++
-        }
-        return {
-            stu,
-            a,
-            changeA
-        }
-    }
-}
+<script setup>
+import { ref, reactive } from 'vue';
+/* 
+    reactive()
+        -返回一个对象的响应式代理
+        -返回的是一个深层响应式对象
+        -也可以使用shallowReactive()创建一个浅层响应式对象
+        -缺点：
+            -只能返回对象的响应式代理
+ */
+/* 
+    ref在生成响应式代理时，它将值包装为了一个对象，需要用.value来获取
+    在模板中，ref对象会被自动解包
+    vue提供给我们一个语法糖，使ref对象在script标签中也能自动解包
+    $是一个实验性的，需要在vite插件做一些中配置 reactivityTransform:true
+    尽量别使用
+ */
+const stu = reactive({
+    name: "孙悟空",
+    age: 18
+})
+console.log(stu);
+let a = $ref(10)
+console.log(a);
 </script>
