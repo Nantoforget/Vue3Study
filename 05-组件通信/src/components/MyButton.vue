7e7ae99b484e8f8fb97fc56ab66e043a5fae0c8d
jiazeshuai <2896613845@qq.com>
2023-03-26 19:37:11 +0800

    slot插槽

---
+++ 05-组件通信/src/components/MyButton.vue
@@ -0,0 +1,12 @@
+<template>
+    <button>
+        <!-- slot标签，会被组件的标签体所替换 -->
+        <slot></slot>
+    </button>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
---
+++ 05-组件通信/src/components/MyWrapper.vue
@@ -0,0 +1,10 @@
+<template>
+    <slot name="aa"></slot>
+    <slot name="bb"></slot>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
