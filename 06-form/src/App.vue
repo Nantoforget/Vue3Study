2596874e77d28e355747b8e7355947573711b906
jiazeshuai <2896613845@qq.com>
2023-03-27 00:11:56 +0800

    学生列表练习

--- 06-form/src/App.vue
+++ 06-form/src/App.vue
@@ -1,32 +1,40 @@
 <template>
-    <h1>哈哈哈</h1>
-    <form @submit.prevent="submitHandler">
-        <!-- 
-            利用v-bind:value和v-on:input实现了双向绑定
-            v-model的原理就是v-bind:value和v-on:input
-        -->
-        <!-- 
-            v-model修饰符
-                .lazy 使用change代替input来处理数据
-                .trim 去除前后的空格
-                .number 将数据转换为数值(合法的数值)
-        -->
-        <input type="text" :value="text" @input="(event) => (text = event.target.value)" />
-        <input type="text" v-model="text" />
-        <div>
-            是否:<input type="checkbox" v-model="bool" true-value="是" false-value="否">
-        </div>
-        <button>提交 </button>
-    </form>
+    <StudentList :stu="students"></StudentList>
 </template>
 
 <script setup>
-import { ref } from "vue"
-let text = ref("");
-let bool = ref(false)
-const submitHandler = () => {
-    console.log(text.value);
-};
+import { ref, reactive } from "vue"
+import StudentList from "./components/StudentList.vue"
+const students = reactive([
+    {
+        id: 1,
+        name: "孙悟空",
+        age: 18,
+        gender: "男",
+        address: "花果山"
+    },
+    {
+        id: 2,
+        name: "猪八戒",
+        age: 28,
+        gender: "男",
+        address: "高老庄"
+    },
+    {
+        id: 3,
+        name: "沙和尚",
+        age: 38,
+        gender: "男",
+        address: "流沙河"
+    },
+    {
+        id: 4,
+        name: "唐僧",
+        age: 16,
+        gender: "男",
+        address: "女儿国"
+    },
+])
 </script>
 
 <style scoped></style>
---
+++ 06-form/src/components/StudentList.vue
@@ -0,0 +1,66 @@
+<template>
+    <table>
+        <caption>学生列表</caption>
+        <thead>
+            <tr>
+                <th>学号</th>
+                <th>姓名</th>
+                <th>年龄</th>
+                <th>性别</th>
+                <th>住址</th>
+                <th>操作</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(stu, index) in students" :key="stu.id">
+                <td>{{ stu.id }}</td>
+                <td>{{ stu.name }}</td>
+                <td>{{ stu.age }}</td>
+                <td>{{ stu.gender }}</td>
+                <td>{{ stu.address }}</td>
+                <td><a @click.prevent="deleteDate(index)" href="#">删除</a></td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script setup>
+// 通过defineProps定义的属性在$attrs中就不存在了
+//定义props属性，接受父组件传递的数据
+const props = defineProps(["stu"])
+const students = props.stu;
+//删除
+const deleteDate = (index) => {
+    // const a = students.find((item) => {
+    //     return item.id == id
+    // })
+    if (confirm("请确认删除")) {
+        students.splice(index, 1)
+    }
+
+}
+</script>
+
+<style scoped>
+table {
+    width: 50%;
+    border-collapse: collapse;
+    margin: 0 auto;
+    font-size: 28px;
+}
+
+caption {
+    font-size: 30px;
+    font-weight: bold;
+}
+
+th,
+td {
+    border: 2px solid red;
+    text-align: center;
+}
+
+a {
+    text-decoration: none;
+}
+</style>
