046bcfd8b21167c6fe1dc51c6a40927055d0f110
jiazeshuai <2896613845@qq.com>
2023-03-26 20:49:28 +0800

    修饰符

--- 05-组件通信/src/App.vue
+++ 05-组件通信/src/App.vue
@@ -1,26 +1,42 @@
 <template>
-    <h1>{{ count }}</h1>
-    <!-- 
-        绑定事件的两种方式
-            1.内联事件处理器(可以用$event作为参数来传递事件对象)
-                -事件触发时，直接执行js语句
-            2.方法事件处理器(vue帮我们传了事件对象event)
-                -事件触发时，vue会对时间的函数进行调用
-            vue如何区分两种处理器：
-                检查事件的值是否合法的js标识符或属性访问路径，
-                    是，则表示它是方法事件处理器
-                    否，表示它是内联事件处理器
-    -->
-    <button @click="count++">Click Me</button>
-    <button @click="change($event)">Click Me</button>
+    <div class="box1" @click.stop="handler('box1')">
+        <div class="box2" @click.stop="handler('box2')">
+            <div class="box3" @click.stop="handler('box3')"></div>
+        </div>
+    </div>
 </template>
 
 <script setup>
-import { ref } from "vue"
-let count = ref(0)
-const change = (event) => {
-    console.log(event);
+/* 
+    时间修饰符：
+        .stop停止时间的传播
+        .capture在捕获阶段触发事件
+        .prevent取消默认行为
+        .self只有事件由自身触发时有效
+        .once绑定一次性的事件
+        .passive主要提升滚动事件的性能
+ */
+const handler = (box) => {
+    alert(box);
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.box1 {
+    width: 300px;
+    height: 300px;
+    background-color: blueviolet;
+}
+
+.box2 {
+    width: 200px;
+    height: 200px;
+    background-color: blue;
+}
+
+.box3 {
+    width: 100px;
+    height: 100px;
+    background-color: aqua;
+}
+</style>
