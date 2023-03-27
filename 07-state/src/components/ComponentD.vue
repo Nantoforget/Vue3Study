8ffdc4a60f86f9af889474bf4c97e3bcd0fa1513
jiazeshuai <2896613845@qq.com>
2023-03-27 17:03:14 +0800

    store订阅

--- 07-state/src/components/ComponentD.vue
+++ 07-state/src/components/ComponentD.vue
@@ -29,27 +29,43 @@
         如果非要使用解构，可以使用pinia中的storeToRefs
         计算属性也有这样的问题(computed)
         storeToRefs只能解构state(ref)和getters(computed)使其变成响应式的，action(function())不能
-     */
-    /* 
+
         state的修改:
             1.直接修改
             2.通过$patch传对象的方式
             3.通过$patch传函数的方式
             4.stuStore.$reset()重置，将数据恢复原始状态
-     */
+    */
     const { name, age, gender, address, title } = storeToRefs(
         useStudentStore()
     );
     const abc = () => {
-        /* stuStore.$patch({
-            name: "哈哈哈",
-            age: 789,
-        }); */
+        // stuStore.$patch({
+        //     name: "哈哈哈",
+        //     age: 789,
+        // });
         //上面的等价于
         stuStore.$state = { name: "哈哈哈", age: 789 };
         //函数的方式
         // stuStore.$patch((state) => {});
     };
+    /* 
+        store的订阅
+            - 当store中的state发生变化时，做一些相应的操作
+            - store.$subscribe(函数,配置对象{ detached: true} :决定组件没有被挂载时是否触发订阅(true:是，false:否))
+                函数有两个参数(mutation,state)
+                    mutation
+                        表示修改的信息
+                    state
+                        是当前修改的数据对象
+                        在使用订阅时，不要在回调函数中直接修改state
+    */
+    stuStore.$subscribe(
+        (mutation, state) => {
+            console.log(100);
+        },
+        { detached: true }
+    );
 </script>
 
 <style scoped></style>
