3ec3487b2bbc63bdb1c762326a1fc569f942ac2a
jiazeshuai <2896613845@qq.com>
2023-04-11 00:51:35 +0800

    .

--- 08-练习/src/components/D.vue
+++ 08-练习/src/components/D.vue
@@ -1,16 +1,8 @@
 <template>
     <form>
         <div> 姓名：<input type="text" v-model="stu.newStu.name" /> </div>
-        <div>
-            年龄：<input type="number" min="1" v-model="stu.newStu.age" />
-        </div>
-        <div>
-            性别： 男<input
-                type="radio"
-                value="男"
-                v-model="stu.newStu.gender" />
-            女<input value="女" type="radio" v-model="stu.newStu.gender" />
-        </div>
+        <div> 年龄：<input type="number" min="1" v-model="stu.newStu.age" /> </div>
+        <div> 性别： 男<input type="radio" value="男" v-model="stu.newStu.gender" /> 女<input value="女" type="radio" v-model="stu.newStu.gender" /> </div>
         <div> 地址：<input type="text" v-model="stu.newStu.address" /> </div>
         <button @click.prevent="addStu">提交</button>
     </form>
