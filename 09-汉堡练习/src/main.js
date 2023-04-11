8b15ccf79c1f83a6bc4a7ebf2f864c2fbd02a3b5
jiazeshuai <2896613845@qq.com>
2023-04-12 01:25:57 +0800

    pinia

--- 09-汉堡练习/src/main.js
+++ 09-汉堡练习/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "@/assets/main.css";
+//引入pinia
 import { createPinia } from "pinia";
 const app = createApp(App);
 const pinia = createPinia();
