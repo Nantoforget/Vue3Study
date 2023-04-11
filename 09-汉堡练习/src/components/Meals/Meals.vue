14596a7f6028d4a579f08fc905d93757fb0cd801
jiazeshuai <2896613845@qq.com>
2023-04-12 01:36:18 +0800

    ==不报错

--- 09-汉堡练习/src/components/Meals/Meals.vue
+++ 09-汉堡练习/src/components/Meals/Meals.vue
@@ -3,7 +3,7 @@
     <div class="meals">
         <!-- 汉堡的信息 -->
         <Meal :meal="item" v-for="item in meals.filterMeals" :key="item.id"></Meal>
-        <h2 v-if="meals.filterMeals.length === 0">没有找到指定的商品</h2>
+        <h2 v-if="meals.filterMeals.length == 0">没有找到指定的商品</h2>
     </div>
 </template>
 
